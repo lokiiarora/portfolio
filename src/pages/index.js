f3ae7c674ceffdb52c43bebbfb485480be10ae1f
Lokesh Raj Arora <lokesh.slg06@gmail.com>
2024-03-16 06:40:11 +0530

    feat: add interests section fix: add about section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,17 @@
+// @ts-check
 import React from "react";
 import {
   AboutSection,
   HeroSection,
   Page,
+  ProjectsSection,
   Seo,
 } from "gatsby-theme-portfolio-minimal";
 import { Animation } from "gatsby-theme-portfolio-minimal/src/components/Animation";
 import { Section } from "gatsby-theme-portfolio-minimal/src/components/Section";
 import { SocialProfiles } from "gatsby-theme-portfolio-minimal/src/components/SocialProfiles/index";
 import { useLocalDataSource } from "gatsby-theme-portfolio-minimal/src/sections/Contact/data";
+// @ts-ignore
 import * as classes from "gatsby-theme-portfolio-minimal/src/sections/Contact/style.module.css";
 
 export default function IndexPage() {
@@ -19,7 +22,7 @@ export default function IndexPage() {
         <HeroSection sectionId="hero" />
         <AboutSection sectionId="about" heading="About me" />
         {/* <InterestsSection sectionId="details" heading="Details" /> */}
-        {/* <ProjectsSection sectionId="features" heading="Built-in Features" /> */}
+        <ProjectsSection sectionId="projects" heading="Projects that I worked on" />
         <ContactSection sectionId="contact" heading="Contact me" />
       </Page>
     </>
